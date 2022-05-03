91f708a06be5885f049d2cc33012f3ce29ce3196
Matt Pityo <mpityo@gmail.com>
2022-05-03 14:48:56 -0400

    nav tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,30 @@
+import { render, cleanup, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        render(<Nav />);
+        expect(screen.getByLabelText('camera')).toHaveTextContent('📸');
+    })
+});
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        render(<Nav />);
+        expect(screen.getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(screen.getByTestId('about')).toHaveTextContent('About me');
+    })
+});
