9528e0fa2624ce2517a24fb662f2c01e3f1a37d7
Matt Pityo <mpityo@gmail.com>
2022-05-04 11:18:55 -0400

    contact component with form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,24 @@
+function ContactForm () {
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form">
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <input type="message" name="message" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm;
